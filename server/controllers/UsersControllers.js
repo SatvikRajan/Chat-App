63b1500effa89377ae561355d70f54230281e315
Satvik Rajan <satvikrajan@gmail.com>
2023-08-16 08:15:08 +0530

    update

--- server/controllers/UsersControllers.js
+++ server/controllers/UsersControllers.js
@@ -7,10 +7,10 @@ module.exports.register = async (req,res,next) =>{
     const{username,email,password} = req.body;
     let usernameCheck = await User.findOne({username})
     if(usernameCheck)
-    return res.status(409).json({error:"Username already used"})
+    return res.json({ msg: "Username already used", status: false });
     let emailCheck = await User.findOne({email})
     if(emailCheck)
-    return res.status(409).json({error: "Email already exist"})
+    return res.json({ msg: "Email already used", status: false });
     const hashedPassword = bcrypt.hashSync(password,saltRounds)
     const user = await User.create({
         email,username,password: hashedPassword
@@ -26,10 +26,10 @@ module.exports.login = async (req,res,next) =>{
      const{username,password} = req.body;
      const user = await User.findOne({username})
      if(!user)
-     return res.status(409).json({error:"Incorrect username or password"})
+     return res.json({ msg: "Incorrect Username or Password", status: false });
      const isPasswordValid = bcrypt.compareSync(password,user.password)
      if(!isPasswordValid)
-     return res.status(409).json({error:"Incorrect username or password"})
+     return res.json({ msg: "Incorrect Username or Password", status: false });
     return res.json({status: true,user})
     }
     catch(ex){

8cb51394ea9b05d784e1396966e0fd661b2c888d
Satvik Rajan <satvikrajan@gmail.com>
2023-10-13 15:25:30 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,41 +1,77 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
-const userRoutes = require('../server/routes/UserRoutes')
-const messageRoute = require('./routes/MessagesRoute')
-const socket = require('socket.io')
-require("dotenv").config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const http = require('http'); 
+const socketIo = require('socket.io');
+const path = require('path');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.json())
-app.use("/api/auth",userRoutes)
-app.use("/api/messages",messageRoute)
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const userRoutes = require('./routes/UserRoutes');
+const messageRoute = require('./routes/MessagesRoute');
+
+app.use('/api/auth', userRoutes);
+app.use('/api/messages', messageRoute);
+
+const dbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chat';
+
 async function main() {
-    await mongoose.connect('https://vercel.com/satvikrajan/chat-app/BJusvcBUu6VfcEhY69TkPNQYrERv');
-    console.log('DB connected succesfully')
+  try {
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB connected successfully');
+  } catch (err) {
+    console.error(err);
+  }
 }
-main() .catch((err)=>{
-    console.log(err)
-})
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
-const io = socket(server,{
-  cors:{origin:'https://vercel.com/satvikrajan/chat-app/BJusvcBUu6VfcEhY69TkPNQYrERv',
-  credentials: true}
-})
+main();
 
-global.onlineUsers = new Map()
-io.on('connection',socket=>{
-  global.chatSocket = socket;
-  socket.on('add-user',(userId)=>{
-    onlineUsers.set(userId,socket.id)
-  })
-  socket.on("send-msg",(data)=>{
-    const sendUserSocket = onlineUsers.get(data.to)
-    if(sendUserSocket){
-      socket.to(sendUserSocket).emit('msg-recieve',data.message)
+if (process.env.NODE_ENV === 'production') {
+  const clientBuildPath = path.resolve(__dirname, 'client', 'build');
+
+  app.use(express.static(clientBuildPath));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(clientBuildPath, 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Running');
+  });
+}
+
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000', 
+    credentials: true,
+  },
+});
+
+const onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+  socket.on('add-user', (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on('send-msg', (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('msg-recieve', data.message);
     }
-  })
-})
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

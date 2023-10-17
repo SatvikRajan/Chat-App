a15e9bce722c42096aaebbd75c6fbe1510b619c9
SatvikRajan <108474135+SatvikRajan@users.noreply.github.com>
2023-10-17 10:36:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,49 +1,60 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
-const userRoutes = require('../server/routes/UserRoutes')
-const messageRoute = require('./routes/MessagesRoute')
-const socket = require('socket.io')
-require("dotenv").config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const userRoutes = require('../server/routes/UserRoutes');
+const messageRoute = require('./routes/MessagesRoute');
+const socket = require('socket.io');
+require("dotenv").config();
 
 app.use(cors({
-    origin: ["https://chat-app-gamma-beryl.vercel.app"],
-    method: ["POST","GET"],
+    origin: ["https://chat-app-gamma-beryl.vercel.app", "https://chat-app-k8gr.vercel.app"],
+    methods: ["POST", "GET"],
     credentials: true
-}
-            ))
-app.use(express.json())
-app.use("/api/auth",userRoutes)
-app.use("/api/messages",messageRoute)
+}));
+
+app.use(express.json());
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
 async function main() {
-  try {
-    await mongoose.connect('mongodb+srv://satvikrajan:Satvik2003@cluster0.lowxabl.mongodb.net/chat-app?retryWrites=true&w=majority');
-    console.log('DB connected successfully');
-  } catch (err) {
-    console.error('Database connection error:', err);
-  }
-}
-main() .catch((err)=>{
-    console.log(err)
-})
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
-const io = socket(server,{
-  cors:{origin:'https://chat-app-gamma-beryl.vercel.app','https://chat-app-k8gr.vercel.app'
-  credentials: true}
-})
-global.onlineUsers = new Map()
-io.on('connection',socket=>{
-  global.chatSocket = socket;
-  socket.on('add-user',(userId)=>{
-    onlineUsers.set(userId,socket.id)
-  })
-  socket.on("send-msg",(data)=>{
-    const sendUserSocket = onlineUsers.get(data.to)
-    if(sendUserSocket){
-      socket.to(sendUserSocket).emit('msg-recieve',data.message)
+    try {
+        await mongoose.connect('mongodb+srv://satvikrajan:Satvik2003@cluster0.lowxabl.mongodb.net/chat-app?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('DB connected successfully');
+    } catch (err) {
+        console.error('Database connection error:', err);
     }
-  })
-})
+}
+
+main().catch((err) => {
+    console.log(err);
+});
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+    cors: {
+        origin: ['https://chat-app-gamma-beryl.vercel.app', 'https://chat-app-k8gr.vercel.app'],
+        credentials: true
+    }
+});
+
+const onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+    global.chatSocket = socket;
+    socket.on('add-user', (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-receive', data.message);
+        }
+    });
+});
